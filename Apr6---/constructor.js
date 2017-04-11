e970cb12d81c5f73785a2c93880c99c80cb2d92a
singh <webexpertsingh@gmail.com>
2017-04-11 18:37:45 -0400

    classwork node properties inheritence

---
+++ Apr6---/constructor.js
@@ -0,0 +1,18 @@
+function User(first_name, last_name, email) {
+
+  //var o = {};
+
+  // Set o's first_name...
+  this.first_name = first_name;
+
+  // Set o's last_name...
+  this.last_name = last_name;
+
+  // Set o's email...
+  this.email = email;
+
+  // Return the object
+  //	return;
+}
+
+console.log(new User("Daljit", "Singh", "daljit@singh.com"));
---
+++ Apr6---/demo.js
@@ -0,0 +1,14 @@
+// var user = {
+// 	name: 'daljit',
+// 	lname: 'singh'
+// }
+// console.log(typeof user.toString());
+// // -----------------------------------------------------
+
+function Animal(hungry, sleep, bored, age){
+	this.hungry = hungry;
+	this.sleep = sleep;
+	this.bored = bored;
+	this.age = age;
+}
+
